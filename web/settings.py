a89b1972ead57afdf2363ccc8a0375e2c41c40b9
meng950813 <1752397568@qq.com>
2020-03-28 16:36:37 +0800

    by meng 将CONST_DICT 中的内容转移到 setting中

--- web/settings.py
+++ web/settings.py
@@ -30,3 +30,12 @@ configuration = {
     'testing': TestingConfig,
     'production': ProductionConfig
 }
+
+PATENT_TYPE = {
+    #
+    1: "patent",  # 发明专利
+    2: "utility_model_patent",  # 实用新型专利
+    3: "design_patent",  # 外观设计
+    8: "other_patent",  # 其他知识产权
+    9: "other_patent"  # 其他知识产权
+}
