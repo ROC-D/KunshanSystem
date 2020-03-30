7595c44e3cb9e9ad9e3e2a3ad7e2c90fbc9b3521
meng950813 <1752397568@qq.com>
2020-03-30 20:49:31 +0800

    添加获取提交记录的函数

--- tests/test_service_provider_dao.py
+++ tests/test_service_provider_dao.py
@@ -4,23 +4,30 @@ import os
 from web.dao.service_provider import *
 
 
-def test_get_departments(init_test_app):
-    department_ids = [1]
-    departments = get_departments(department_ids)
+# def test_get_departments(init_test_app):
+#     department_ids = [1]
+#     departments = get_departments(department_ids)
+#
+#     assert len(departments) == 1
+#     department = departments[0]
+#     assert 'id' in department
+#     assert 'department' in department
+#
+#
+# def test_get_assignments_by_charger(init_test_app):
+#     charger_id = 1
+#     tasks = get_assignments(charger_id)
+#
+#     assert len(tasks) != 0
+#     for task in tasks:
+#         assert 'type' in task
 
-    assert len(departments) == 1
-    department = departments[0]
-    assert 'id' in department
-    assert 'department' in department
 
-
-def test_get_assignments_by_charger(init_test_app):
-    charger_id = 1
-    tasks = get_assignments(charger_id)
-
-    assert len(tasks) != 0
-    for task in tasks:
-        assert 'type' in task
+def test_get_records(init_test_app):
+    records = get_records(charger_id=1)
+    print(records)
+    for record in records:
+        assert "submit_time" in record
 
 
 if __name__ == '__main__':
